0b0a87608eccea82369ba0786ec2149b3eef5ca8
TB831 <druu831@gmail.com>
2018-10-15 14:15:06 -0700

    Game almost complete. Minor bug present

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,40 +1,21 @@
 import React from "react";
-// import ReactCardFlip from 'react-card-flip';
 import '../Card/card.css';
+import backCardImage from '../../assets/playing-card-back.png';
 
 const Card = ({ card, index, cardHandleClick, display }) => {
+
   return (
+    <div className='dib'>
+      <div className='card-front'></div>
         <img
-          alt='card back'
-          className='dib br3 pr3 ma2 grow bw2 tc'
-          src={card.image}
-          height='160px'
-          width='90px'
-          display={display}
-          onClick={() => cardHandleClick(index)}
-        ></img>
-    // <div className='dib'>
-      // <ReactCardFlip isFlipped={display}>
-      //   <img
-      //     alt='card back'
-      //     key='front'
-      //     className='br3 pr3 ma2 grow bw2 tc'
-      //     src={cardImage}
-      //     height='160px'
-      //     width='90px'
-      //     onClick={() => cardHandleClick(index)}
-      //   ></img>
-      //   <img
-      //     alt='card back'
-      //     key='back'
-      //     className='br3 pr3 ma2 grow bw2 tc'
-      //     src={require('../assets/playing-card-back.png')}
-      //     height='160px'
-      //     width='90px'
-      //     onClick={() => cardHandleClick(index)}
-      //   ></img>
-      // </ReactCardFlip>
-    // </div>
+            alt='card back'
+            className='dib br3 pr3 ma2 grow bw2 tc'
+            src={display ? card.image : backCardImage}
+            height='160px'
+            width='90px'
+            onClick={() => cardHandleClick(index)}
+          ></img>
+    </div>
   )
 }
 
